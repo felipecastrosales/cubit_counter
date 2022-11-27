795deb4dedbd3653fb2ee52ca4f730341b1b3ff4
Felipe Sales <soufeliposales@gmail.com>
2022-11-27 14:34:54 -0300

    test: init the cubit test with initial state and increment methods

---
+++ test/counter/cubit/counter_cubit_test.dart
@@ -0,0 +1,38 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:cubit_counter/counter/cubit/cubit.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('CounterCubit', () {
+    test('initial state is 0', () {
+      expect(CounterCubit().state, 0);
+    });
+
+    group('increment', () {
+      blocTest<CounterCubit, int>(
+        'emits [1] when state is 0',
+        build: CounterCubit.new,
+        act: (cubit) => cubit.increment(),
+        expect: () => const <int>[1],
+      );
+
+      blocTest<CounterCubit, int>(
+        'emits [1, 2, 3] when state is 0',
+        build: CounterCubit.new,
+        act: (cubit) => cubit
+          ..increment()
+          ..increment()
+          ..increment(),
+        expect: () => const <int>[1, 2, 3],
+      );
+
+      blocTest<CounterCubit, int>(
+        'emits [16] when state is 15',
+        build: CounterCubit.new,
+        seed: () => 15,
+        act: (cubit) => cubit.increment(),
+        expect: () => const <int>[16],
+      );
+    });
+  });
+}
